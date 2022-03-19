de3550ac7230bdaf4e782f511916b930e3d8dad4
iqpizza6349 <iqpizza6349@gmail.com>
2022-03-20 00:34:53 +0900

    ⚙️:: Setup

---
+++ src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/SchoolmealBackSetupApplication.java
@@ -0,0 +1,13 @@
+package kr.hs.dgsw.cns.schoolmealbacksetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolmealBackSetupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolmealBackSetupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/hs/dgsw/cns/schoolmealbacksetup/SchoolmealBackSetupApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hs.dgsw.cns.schoolmealbacksetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolmealBackSetupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
