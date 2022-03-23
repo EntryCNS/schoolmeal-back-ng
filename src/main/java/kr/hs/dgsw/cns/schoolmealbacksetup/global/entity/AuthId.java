178c7cfd30b0a29909630784e3c8728dd306cf32
황지민 <80872558+krapoi@users.noreply.github.com>
2022-03-23 15:32:12 +0900

    ♻:: Refactoring CommonUser 엔티티#4

--- src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/global/entity/AuthId.java
+++ src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/global/entity/AuthId.java
@@ -2,6 +2,7 @@ package kr.hs.dgsw.cns.schoolmealbacksetup.global.entity;
 
 import kr.hs.dgsw.cns.schoolmealbacksetup.domain.user.entity.User;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
@@ -9,14 +10,11 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import java.io.Serializable;
 
+@Getter
 @Embeddable
 @AllArgsConstructor @NoArgsConstructor
 public class AuthId implements Serializable {
     @OneToOne
     @JoinColumn(name = "user_id")
     private User user;
-
-    public User getUser(){
-        return user;
-    }
 }
