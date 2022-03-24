aca6b6636acc51ffbf1ffc277897c6231b001af3
VINTO1819 <develretr0@gmail.com>
2022-03-24 13:09:33 +0900

    🔧:: @Id에서 @EmbeddedId로 변경

--- src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/domain/menu/entity/Vote.java
+++ src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/domain/menu/entity/Vote.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter
 @Builder
@@ -16,6 +13,6 @@ import javax.persistence.Id;
 @NoArgsConstructor
 @Entity
 public class Vote {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EmbeddedId
     private VoteId id;
 }
