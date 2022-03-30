509b4b34853c440991aed81d324a6e62e2d88de8
iqpizza6349 <iqpizza6349@gmail.com>
2022-03-30 13:12:35 +0900

    🔀:: #17 PR 코드리뷰 반영, 리포지토리 변경

--- src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/domain/auth/repository/OAuthRepository.java
+++ src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/domain/auth/repository/OAuthRepository.java
@@ -2,7 +2,8 @@ package kr.hs.dgsw.cns.schoolmealbacksetup.domain.auth.repository;
 
 import kr.hs.dgsw.cns.schoolmealbacksetup.domain.auth.entity.OAuthUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface OAuthRepository extends JpaRepository<OAuthUser, Long> {
+public interface OAuthRepository extends CrudRepository<OAuthUser, Long> {
 
 }
