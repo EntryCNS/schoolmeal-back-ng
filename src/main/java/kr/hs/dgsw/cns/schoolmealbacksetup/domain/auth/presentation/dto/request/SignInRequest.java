6ea87a690a63f6f8a083d7cdc7fd08459bedda41
qkrthdud002 <qkrthdud002@gmail.com>
2022-04-13 19:34:30 +0900

    💥:: OAuth 사용자 추가

--- src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/domain/auth/presentation/dto/request/SignInRequest.java
+++ src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/domain/auth/presentation/dto/request/SignInRequest.java
@@ -10,7 +10,7 @@ import javax.validation.constraints.Size;
 
 @Getter
 @AllArgsConstructor
-public class SignIn {
+public class SignInRequest {
 
     @NotNull
     @Size(min=3, max=20)
--- src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/domain/auth/presentation/dto/response/SignInResponse.java
+++ src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/domain/auth/presentation/dto/response/SignInResponse.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @Getter
 @Builder
 @AllArgsConstructor
-public class SignIn {
+public class SignInResponse {
 
     private String id;
     private String password;
