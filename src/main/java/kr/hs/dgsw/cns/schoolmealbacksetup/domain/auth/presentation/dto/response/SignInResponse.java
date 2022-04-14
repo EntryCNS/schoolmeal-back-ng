e231c6f5c49d32c395d7c9a377818b40b8fd71d8
qkrthdud002 <qkrthdud002@gmail.com>
2022-04-14 12:33:05 +0900

    SignInResponse 수정

--- src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/domain/auth/presentation/dto/response/SignInResponse.java
+++ src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/domain/auth/presentation/dto/response/SignInResponse.java
@@ -1,15 +1,16 @@
 package kr.hs.dgsw.cns.schoolmealbacksetup.domain.auth.presentation.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
-@AllArgsConstructor
 public class SignInResponse {
 
-    private String refresh_token;
+    @JsonProperty("refresh_token")
+    private String refreshToken;
 
 
 }
