f6731cfd82fe452c0a79265f5c896cebd060b367
황지민 <80872558+krapoi@users.noreply.github.com>
2022-04-14 09:25:34 +0900

    🔀::(#32) 리뷰 추가 dto 생성

---
+++ src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/domain/review/dto/response/WriteReviewResponseDto.java
@@ -0,0 +1,27 @@
+package kr.hs.dgsw.cns.schoolmealbacksetup.domain.review.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class WriteReviewResponseDto {
+
+    @NotNull
+    private String date;
+
+    @NotNull
+    private enum review_rime {BREAKFAST, LUNCH, DINNER};
+
+    @NotNull
+    private String massage;
+
+    @NotNull
+    @Size(max = 5)
+    private int rate;
+}
