94148cae6234ad575f3776340c35e0d0ae67c915
황지민 <80872558+krapoi@users.noreply.github.com>
2022-04-15 14:14:40 +0900

    ::(#32) request

--- src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/domain/review/dto/request/WriteReviewRequestDto.java
+++ src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/domain/review/dto/request/WriteReviewRequestDto.java
@@ -12,7 +12,7 @@ import javax.validation.constraints.NotNull;
 @Getter
 @Builder
 @AllArgsConstructor
-public class WriteReviewResponseDto {
+public class WriteReviewRequestDto {
 
     @NotNull
     private String date;
