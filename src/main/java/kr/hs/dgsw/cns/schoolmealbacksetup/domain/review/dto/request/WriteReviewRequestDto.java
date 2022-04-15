18a2dcafca951cf129ddb295f1ac3395e37926d4
황지민 <80872558+krapoi@users.noreply.github.com>
2022-04-15 14:16:00 +0900

    🔀::(#32) 리뷰 추가 dto 생성
    * 🔀::(#32) 리뷰 추가 dto 생성
    
    * 🔀::(#32) 리뷰 추가 dto 생성
    
    * 🔀::(#32) 리뷰 추가 dto 생성
    
    * 🔀::(#32) 리뷰 추가 dto 생성
    
    * 🔀::(#32) 리뷰 추가 dto 생성
    
    * 🔀::(#32) 리뷰 추가 dto 생성
    
    * ::(#32) Add Min
    
    * ::(#32) request
    
    * ::(#32) request

---
+++ src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/domain/review/dto/request/WriteReviewRequestDto.java
@@ -0,0 +1,30 @@
+package kr.hs.dgsw.cns.schoolmealbacksetup.domain.review.dto.request;
+
+import kr.hs.dgsw.cns.schoolmealbacksetup.domain.review.type.ReviewTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class WriteReviewRequestDto {
+
+    @NotNull
+    private String date;
+
+    @NotNull
+    private ReviewTime reviewTime;
+
+    @NotNull
+    private String massage;
+
+    @NotNull
+    @Max(5)
+    @Min(1)
+    private int rate;
+}
