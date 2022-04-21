a6cd5ed6eb9b8e10ec9bdffa595900214f678f2c
임재청 <iqpizza6349@gmail.com>
2022-04-21 20:22:18 +0900

    🔀::(#45) ORM 중 발생한 오류를 해결함 (#47)
    * 🔧::(#45) ORM 중 발생한 오류를 해결함 #46 참고
    
    * 🔀::(#45) #14 merge에 삭제되지않은 클래스 삭제

--- src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/domain/user/entity/AuthId.java
+++ src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/domain/user/entity/AuthId.java
@@ -1,6 +1,7 @@
 package kr.hs.dgsw.cns.schoolmealbacksetup.domain.user.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
@@ -8,14 +9,11 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import java.io.Serializable;
 
+@Getter
 @Embeddable
 @AllArgsConstructor @NoArgsConstructor
 public class AuthId implements Serializable {
     @OneToOne
     @JoinColumn(name = "user_id")
     private User user;
-
-    public User getUser(){
-        return user;
-    }
 }
