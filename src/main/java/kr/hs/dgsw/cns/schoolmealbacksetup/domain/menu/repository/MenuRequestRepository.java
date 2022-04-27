168604a66bf17ea13cfb947f09c237434449e59e
임재청 <iqpizza6349@gmail.com>
2022-04-27 16:43:50 +0900

    🔀::(#48) 리포지토리 어노테이션 추가

--- src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/domain/menu/repository/MenuRequestRepository.java
+++ src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/domain/menu/repository/MenuRequestRepository.java
@@ -2,6 +2,8 @@ package kr.hs.dgsw.cns.schoolmealbacksetup.domain.menu.repository;
 
 import kr.hs.dgsw.cns.schoolmealbacksetup.domain.menu.entity.MenuRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MenuRequestRepository extends JpaRepository<MenuRequest, Long> {
 }
