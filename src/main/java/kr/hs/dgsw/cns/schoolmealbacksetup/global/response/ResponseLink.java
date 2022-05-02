aeb045a6fdb57af343bbff4d36598b929a6c28fc
qkrthdud002 <qkrthdud002@gmail.com>
2022-05-02 19:51:30 +0900

    🔀::(#52) 미사용 import 삭제

--- src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/global/response/ResponseLink.java
+++ src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/global/response/ResponseLink.java
@@ -1,9 +1,7 @@
 package kr.hs.dgsw.cns.schoolmealbacksetup.global.response;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.springframework.http.HttpMethod;
 
 @Getter
 @AllArgsConstructor
