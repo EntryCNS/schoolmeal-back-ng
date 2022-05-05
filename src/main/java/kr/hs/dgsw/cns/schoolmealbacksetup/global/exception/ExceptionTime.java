d47b82b7e756bda9f47bf0b9c076039659f9d4d8
iqpizza6349 <iqpizza6349@gmail.com>
2022-05-06 00:41:53 +0900

    🖋️:: (#59) 예외 시간 클래스 private-constructor 추가

--- src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/global/exception/ExceptionTime.java
+++ src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/global/exception/ExceptionTime.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 
 public class ExceptionTime {
 
+    private ExceptionTime() {}
+
     public static LocalDateTime getTime() {
         return LocalDateTime.now();
     }
