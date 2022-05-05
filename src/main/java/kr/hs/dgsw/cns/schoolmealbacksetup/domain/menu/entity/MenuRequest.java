a958bf787dfb271b6531f85693d47ae6e16bed5d
iqpizza6349 <iqpizza6349@gmail.com>
2022-05-06 00:56:57 +0900

    ♻:: (#59) GlobalException 예외 클래스 추가

--- src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/domain/menu/entity/MenuRequest.java
+++ src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/domain/menu/entity/MenuRequest.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import kr.hs.dgsw.cns.schoolmealbacksetup.domain.menu.type.MenuCategory;
 import kr.hs.dgsw.cns.schoolmealbacksetup.domain.menu.type.MenuState;
 import kr.hs.dgsw.cns.schoolmealbacksetup.domain.user.entity.User;
-import kr.hs.dgsw.cns.schoolmealbacksetup.global.exception.ExceptionTime;
+import kr.hs.dgsw.cns.schoolmealbacksetup.global.exception.GlobalException;
 import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
@@ -51,16 +51,14 @@ public class MenuRequest {
 
     @Getter
     @ResponseStatus(value = HttpStatus.CONFLICT)
-    public static class PageCannotNegative extends RuntimeException {
-        private final LocalDateTime localDateTime = ExceptionTime.getTime();
+    public static class PageCannotNegative extends GlobalException {
         @JsonProperty(value = "message")
         private static final String MESSAGE = "Parameter 'page' cannot be negative.";
     }
 
     @Getter
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
-    public static class CannotFound extends RuntimeException {
-        private final LocalDateTime localDateTime = ExceptionTime.getTime();
+    public static class CannotFound extends GlobalException {
         private final String message;
 
         public CannotFound(long id) {
