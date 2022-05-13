43afd3778671ef1a8cdcfb96d11d33504a557ec2
황지민 <80872558+krapoi@users.noreply.github.com>
2022-05-13 17:47:44 +0900

    💥::#56 WriteReviewResponseDto 작성 및 ReviewService 수정

--- src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/domain/review/service/ReviewService.java
+++ src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/domain/review/service/ReviewService.java
@@ -1,14 +1,15 @@
 package kr.hs.dgsw.cns.schoolmealbacksetup.domain.review.service;
 
 import kr.hs.dgsw.cns.schoolmealbacksetup.domain.review.presentation.dto.request.WriteReviewRequestDto;
-import kr.hs.dgsw.cns.schoolmealbacksetup.domain.review.presentation.dto.response.ReviewDto;
 import kr.hs.dgsw.cns.schoolmealbacksetup.domain.review.presentation.dto.response.ReviewListDto;
-import kr.hs.dgsw.cns.schoolmealbacksetup.domain.review.presentation.dto.response.WriteReviewDto;
+import kr.hs.dgsw.cns.schoolmealbacksetup.domain.review.presentation.dto.response.WriteReviewResponseDto;
 import kr.hs.dgsw.cns.schoolmealbacksetup.domain.review.type.ReviewTime;
 
 
 public interface ReviewService {
-    WriteReviewRequestDto writeReview(WriteReviewDto writeReview);
+
+    WriteReviewResponseDto writeReview(WriteReviewRequestDto writeReviewRequest);
 
     ReviewListDto getReviewByDate(String date, ReviewTime reviewTime);
+
 }
