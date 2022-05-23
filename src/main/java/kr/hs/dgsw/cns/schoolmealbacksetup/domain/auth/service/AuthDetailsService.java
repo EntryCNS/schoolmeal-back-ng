2544f04a1a616f55792601842bb2da7011cacd2d
VINTO1819 <develretr0@gmail.com>
2022-05-23 20:14:33 +0900

    ♻️:: AuthDetailsService 구현

--- src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/domain/auth/service/AuthDetailsService.java
+++ src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/domain/auth/service/AuthDetailsService.java
@@ -1,6 +1,7 @@
 package kr.hs.dgsw.cns.schoolmealbacksetup.domain.auth.service;
 
 import kr.hs.dgsw.cns.schoolmealbacksetup.domain.auth.commonuser.repository.CommonUserRepository;
+import kr.hs.dgsw.cns.schoolmealbacksetup.domain.auth.repository.OAuthRepository;
 import kr.hs.dgsw.cns.schoolmealbacksetup.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,10 +12,13 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class AuthDetailsService implements UserDetailsService {
-    private final CommonUserRepository commonUserRepository;
+    private final OAuthRepository oAuthRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null; // TODO: 추후에 UserDetails를 상속한 AuthDetail 클래스 만들기 + 해당 메서드 구현하기
+        return oAuthRepository.findById(Long.valueOf(username))
+                .orElseThrow(() -> new UsernameNotFoundException("User not exists"))
+                .getUserId()
+                .getUser();
     }
 }
