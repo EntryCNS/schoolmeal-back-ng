2b4b34168ace89387a38696fb7dcef779b2246c6
VINTO1819 <develretr0@gmail.com>
2022-05-23 20:38:38 +0900

    ♻️:: 코드스멜 해결

--- src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/global/security/JwtConfiguration.java
+++ src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/global/security/JwtConfiguration.java
@@ -1,11 +1,8 @@
 package kr.hs.dgsw.cns.schoolmealbacksetup.global.security;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Getter
