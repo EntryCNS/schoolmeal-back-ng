f04c8497ea7cd1dcaaaa02ecb1bb0764f64a236c
VINTO1819 <develretr0@gmail.com>
2022-06-01 11:33:42 +0900

    ♻️:: Mysql 적용

--- src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/domain/auth/service/AuthDetailsService.java
+++ src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/domain/auth/service/AuthDetailsService.java
@@ -1,6 +1,6 @@
 package kr.hs.dgsw.cns.schoolmealbacksetup.domain.auth.service;
 
-import kr.hs.dgsw.cns.schoolmealbacksetup.domain.auth.oauthuser.repository.OAuthRepository;
+import kr.hs.dgsw.cns.schoolmealbacksetup.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,13 +10,12 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class AuthDetailsService implements UserDetailsService {
-    private final OAuthRepository oAuthRepository;
+
+    private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return oAuthRepository.findById(Long.valueOf(username))
-                .orElseThrow(() -> new UsernameNotFoundException("User not exists"))
-                .getUserId()
-                .getUser();
+        return userRepository.findById(Long.valueOf(username))
+                .orElseThrow(() -> new UsernameNotFoundException("User not exists"));
     }
 }
