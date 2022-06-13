c51dbf9a11ef2211c01679416e193ed926dbcc82
VINTO1819 <develretr0@gmail.com>
2022-06-13 11:24:53 +0900

    ♻️:: 테스트를 위해 HashCode 및 Equals 오버라이딩

--- src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/global/response/ResponseLink.java
+++ src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/global/response/ResponseLink.java
@@ -1,10 +1,13 @@
 package kr.hs.dgsw.cns.schoolmealbacksetup.global.response;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
 @AllArgsConstructor
+@EqualsAndHashCode @ToString
 public class ResponseLink {
 
     private String rel;
