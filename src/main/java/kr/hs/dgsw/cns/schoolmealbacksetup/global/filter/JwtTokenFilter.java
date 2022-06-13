6b07134dc8a0aecb0d881ca4e47076c5234c548e
iqpizza6349 <iqpizza6349@gmail.com>
2022-06-14 08:08:53 +0900

    🖋️:: (#78) JWT 토큰 필터 수정 및 TokenException 추가

--- src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/global/filter/JwtTokenFilter.java
+++ src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/global/filter/JwtTokenFilter.java
@@ -1,6 +1,7 @@
 package kr.hs.dgsw.cns.schoolmealbacksetup.global.filter;
 
 import kr.hs.dgsw.cns.schoolmealbacksetup.global.security.JwtProvider;
+import kr.hs.dgsw.cns.schoolmealbacksetup.global.security.TokenException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -18,12 +19,29 @@ public class JwtTokenFilter extends OncePerRequestFilter {
     private final JwtProvider jwtProvider;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String bearerToken = jwtProvider.getTokenFromHeader(request.getHeader("Authorization"));
-        if(bearerToken != null) {
-            Authentication authentication = jwtProvider.getAuthenticationFromToken(bearerToken);
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        try {
+            String bearerToken = jwtProvider.getTokenFromHeader(request.getHeader("Authorization"));
+            if (bearerToken != null) {
+                Authentication authentication = jwtProvider.getAuthenticationFromToken(bearerToken);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+            filterChain.doFilter(request, response);
+        } catch (TokenException e) {
+            filterException(e, response);
         }
-        filterChain.doFilter(request, response);
+    }
+
+    private void filterException(TokenException exception, HttpServletResponse response) {
+        response.setStatus(exception.getStatus().value());
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json");
+        
+        try {
+            response.getWriter().write(exception.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
