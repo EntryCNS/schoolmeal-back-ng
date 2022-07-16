4d2a842684ace0813546b6fedcee3fb59668f729
VINTO1819 <develretr0@gmail.com>
2022-07-16 20:43:48 +0900

    레포지토리 문제 수정

--- src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/domain/menu/repository/MenuRequestRepository.java
+++ src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/domain/menu/repository/MenuRequestRepository.java
@@ -3,7 +3,7 @@ package kr.hs.dgsw.cns.schoolmealbacksetup.domain.menu.repository;
 import kr.hs.dgsw.cns.schoolmealbacksetup.domain.menu.entity.MenuRequest;
 import kr.hs.dgsw.cns.schoolmealbacksetup.domain.menu.type.MenuState;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -14,8 +14,8 @@ import java.time.LocalDateTime;
 public interface MenuRequestRepository extends JpaRepository<MenuRequest, Long> {
 
     @Query("SELECT menu FROM MenuRequest menu WHERE menu.state=:state AND menu.acceptedAt=:date ORDER BY menu.createAt")
-    Page<MenuRequest> findAllByStateAndAcceptedDate(MenuState state, LocalDateTime date, PageRequest pageRequest);
+    Page<MenuRequest> findAllByStateAndAcceptedDate(MenuState state, LocalDateTime date, Pageable pageRequest);
 
     @Query("SELECT menu FROM MenuRequest menu WHERE menu.state=:state ORDER BY menu.createAt")
-    Page<MenuRequest> findAllByState(MenuState state, PageRequest pageRequest);
+    Page<MenuRequest> findAllByState(MenuState state, Pageable pageRequest);
 }
