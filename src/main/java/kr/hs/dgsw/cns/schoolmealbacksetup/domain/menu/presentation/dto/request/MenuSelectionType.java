c54db0558652f7263f55e612afc9152635ec50ff
VINTO1819 <develretr0@gmail.com>
2022-07-16 19:59:03 +0900

    Request Enum 생성

---
+++ src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/domain/menu/presentation/dto/request/MenuSelectionType.java
@@ -0,0 +1,17 @@
+package kr.hs.dgsw.cns.schoolmealbacksetup.domain.menu.presentation.dto.request;
+
+import kr.hs.dgsw.cns.schoolmealbacksetup.domain.menu.type.MenuState;
+
+import java.util.Optional;
+
+public enum MenuSelectionType {
+    ACCEPTED, ACCEPTED_TODAY, DENIED, STANDBY;
+
+    public static MenuSelectionType fromString(String name) {
+        return MenuSelectionType.valueOf(name);
+    }
+
+    public MenuState toEntityType() {
+        return MenuState.valueOf(this.name());
+    }
+}
