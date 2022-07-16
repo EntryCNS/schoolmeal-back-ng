7aaf7a930d0d4e3432f418783495bc4faea6d237
VINTO1819 <develretr0@gmail.com>
2022-07-16 20:01:06 +0900

    레포지토리 쿼리 수동작성

--- src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/domain/menu/repository/MenuRequestRepository.java
+++ src/main/java/kr/hs/dgsw/cns/schoolmealbacksetup/domain/menu/repository/MenuRequestRepository.java
@@ -1,13 +1,21 @@
 package kr.hs.dgsw.cns.schoolmealbacksetup.domain.menu.repository;
 
 import kr.hs.dgsw.cns.schoolmealbacksetup.domain.menu.entity.MenuRequest;
+import kr.hs.dgsw.cns.schoolmealbacksetup.domain.menu.type.MenuState;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.time.LocalDateTime;
 
 @Repository
 public interface MenuRequestRepository extends JpaRepository<MenuRequest, Long> {
 
-    List<MenuRequest> findTop10ByOrderByCreateAtDesc();
+    @Query("SELECT menu FROM MenuRequest menu WHERE menu.state=:state AND menu.acceptedAt=:date ORDER BY menu.createAt")
+    Page<MenuRequest> findAllByStateAndAcceptedDate(MenuState state, LocalDateTime date, PageRequest pageRequest);
+
+    @Query("SELECT menu FROM MenuRequest menu WHERE menu.state=:state ORDER BY menu.createAt")
+    Page<MenuRequest> findAllByState(MenuState state, PageRequest pageRequest);
 }
